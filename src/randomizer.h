b01a7a815dfd55c0dd1f5704986ed14935ec649a
richardolt <richardolt>
2015-01-18 22:13:49 +0100

    updated with changes

--- src/randomizer.h
+++ src/randomizer.h
@@ -1,6 +1,12 @@
 #ifndef RANDOMIZER_H_
 #define RANDOMIZER_H_
 #include <linux/compiler.h> 
+#include <linux/slab.h> 
+#include <linux/fs.h> 
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/random.h>
+#include <asm/uaccess.h>
 
 __must_check int randomizer_init(void);
 
